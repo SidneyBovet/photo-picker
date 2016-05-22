f0e2df95da5dcb617a8518f891d8ce0c9b40a564
Sidney Bovet <sidney.bovet@epfl.ch>
2016-05-22 23:06:33 +0200

    Provides implementation of the DBSCAN algorithm

--- app/src/main/java/ch/epfl/ivrl/photopicker/imageGrouping/ImageDBSCAN.java
+++ app/src/main/java/ch/epfl/ivrl/photopicker/imageGrouping/ImageDBSCAN.java
@@ -1,7 +1,166 @@
 package ch.epfl.ivrl.photopicker.imageGrouping;
 
+import android.util.Log;
+
+import org.ejml.data.DenseMatrix32F;
+import org.ejml.data.Matrix;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ch.epfl.ivrl.photopicker.imageData.Photograph;
+import ch.epfl.ivrl.photopicker.imageData.Scene;
+import ch.epfl.ivrl.photopicker.imageData.Vacation;
+
 /**
  * Created by Sidney on 5/22/2016.
  */
-public class ImageDBSCAN {
+public class ImageDBSCAN implements ImageClusteringTechnique {
+
+    private float mEps; // the minimum distance between two points to be in the same cluster
+    private int mMinPointCount; // the minimum number of points that can form a cluster
+    private Set<DataPoint> mDataSet; // the points to be processed
+
+    /**
+     *
+     * @param eps           The threshold below which two points are considered in the same cluster
+     * @param minPointCount The minimum number of points to form a cluster
+     */
+    public ImageDBSCAN(float eps, int minPointCount) {
+        mEps = eps;
+        mMinPointCount = minPointCount;
+    }
+
+    /**
+     * @param photos                 The list of photographs to cluster among different scenes
+     * @param pairwiseDistanceMatrix the matrix giving how far a is from b, a from c, b from c, etc.
+     * @return A Vacation consisting of different scenes, all having at least one photograph
+     */
+    @Override
+    public Vacation clusterPhotographs(List<Photograph> photos, DenseMatrix32F pairwiseDistanceMatrix) {
+        if (pairwiseDistanceMatrix.getNumRows() != pairwiseDistanceMatrix.getNumCols())
+            throw new IllegalArgumentException("Pairwise distance matrix should be a square matrix");
+        if (photos == null)
+            throw new IllegalArgumentException("Cannot cluster a null list of photographs");
+
+        Set<Set<Integer>> clusters = dbscan(pairwiseDistanceMatrix);
+
+        for (Set<Integer> cluster : clusters) {
+            Log.d("Clustering", "---------------------------");
+            for (Integer p : cluster) {
+                Log.d("Clustering", "" + p);
+            }
+            Log.d("Clustering", "---------------------------");
+        }
+
+        Vacation vacation = new Vacation();
+        for (Set<Integer> cluster : clusters) {
+            Scene scene = new Scene();
+            for (Integer p : cluster) {
+                scene.addPhoto(photos.get(p));
+            }
+            vacation.addScene(scene);
+        }
+
+
+        return vacation;
+    }
+
+    /**
+     * Performs the DBSCAN algorithm using the given pairwise distance matrix. This function is
+     * element-agnostic: it only uses their indices, as in the distance matrix.
+     * @param matrix The pairwise distance matrix of all the elements to be clustered.
+     * @return a set of set of integers, representing a set of clusters of indices.
+     */
+    private Set<Set<Integer>> dbscan(DenseMatrix32F matrix) {
+
+        Set<Set<Integer>> returned = new HashSet<>();
+
+        // initialize data set
+        mDataSet = new HashSet<>();
+        for (int i = 0; i < matrix.getNumCols(); i++) {
+            DataPoint point = new DataPoint(i);
+            mDataSet.add(point);
+        }
+
+        for (DataPoint point : mDataSet) {
+            if (!point.visited) {
+                point.visited = true;
+
+                Set<DataPoint> neighbors = getNeighbors(point.point, matrix);
+
+                Set<Integer> newCluster;
+                if (neighbors.size() < mMinPointCount) {
+                    // photo is not part of any cluster (aka noise)
+                    newCluster = new HashSet<>(1);
+                    newCluster.add(point.point);
+                } else {
+                    newCluster = expandCluster(point, neighbors, matrix);
+                }
+
+                returned.add(newCluster);
+            }
+        }
+
+        return returned;
+    }
+
+    private Set<DataPoint> getNeighbors(Integer p, DenseMatrix32F matrix) {
+        Set<DataPoint> neighbors = new HashSet<>();
+
+        for(int i = 0; i < matrix.getNumCols(); i++) {
+            if(matrix.get(p, i) < mEps)
+                neighbors.add(getDataPoint(i));
+        }
+
+        return neighbors;
+    }
+
+    private Set<Integer> expandCluster(DataPoint p, Set<DataPoint> neighbors, DenseMatrix32F matrix) {
+        Set<Integer> newCluster = new HashSet<>(neighbors.size() + 1);
+
+        p.clustered = true;
+        newCluster.add(p.point);
+
+        for (DataPoint pPrime : neighbors) {
+            if (!pPrime.visited) {
+                pPrime.visited = true;
+                Set<DataPoint> neighborsPrime = getNeighbors(pPrime.point, matrix);
+                if (neighborsPrime.size() >= mMinPointCount) {
+                    //neighbors.addAll(neighborsPrime); // bug?
+                    neighborsPrime.addAll(neighbors);
+                    expandCluster(pPrime, neighborsPrime, matrix);
+                    break; //isok?
+                }
+            }
+            if (!pPrime.clustered) {
+                pPrime.clustered = true;
+                newCluster.add(pPrime.point);
+            }
+        }
+
+        return newCluster;
+    }
+
+    public DataPoint getDataPoint(int point) {
+        for (DataPoint p : mDataSet) {
+            if (p.point == point)
+                return p;
+        }
+
+        throw new IllegalStateException("Point " + point + " does not exist");
+    }
+
+    private class DataPoint {
+
+        public boolean visited = false;
+        public boolean clustered = false;
+        public int point;
+
+        public DataPoint(int point) {
+            this.point = point;
+        }
+    }
 }
