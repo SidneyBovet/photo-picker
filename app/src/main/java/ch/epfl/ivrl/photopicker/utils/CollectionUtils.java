10269c64b9f124951abee395b3d6b6f1acec0333
Sidney Bovet <sidney.bovet@gmail.com>
2016-06-01 17:55:14 +0200

    Adds sorting util

--- app/src/main/java/ch/epfl/ivrl/photopicker/utils/CollectionUtils.java
+++ app/src/main/java/ch/epfl/ivrl/photopicker/utils/CollectionUtils.java
@@ -1,7 +1,17 @@
 package ch.epfl.ivrl.photopicker.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * Created by Sidney on 01.06.2016.
  */
 public class CollectionUtils {
+    public static
+    <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
+        List<T> list = new ArrayList<T>(c);
+        java.util.Collections.sort(list);
+        return list;
+    }
 }
